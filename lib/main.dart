da6cf40fc3d5335853a22c07d30ae7aa76001e51
8thgencore <axelmaker@ya.ru>
2022-05-12 22:38:57 +0300

    Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:photo_gallery_app/provider/gallery_data.dart';
 import 'package:photo_gallery_app/screens/photo_gallery_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: PhotoGalleryScreen(),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => GalleryData()..getImages(),
+      child: MaterialApp(
+        theme: ThemeData.dark(),
+        home: PhotoGalleryScreen(),
+      ),
     );
   }
 }
--- lib/screens/photo_gallery_screen.dart
+++ lib/screens/photo_gallery_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:photo_gallery_app/keys.dart';
-import 'package:photo_gallery_app/services/network_helper.dart';
+import 'package:photo_gallery_app/provider/gallery_data.dart';
+import 'package:provider/provider.dart';
 
 class PhotoGalleryScreen extends StatefulWidget {
   @override
@@ -8,62 +8,27 @@ class PhotoGalleryScreen extends StatefulWidget {
 }
 
 class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
-  Future<List<String>>? images;
-
-  Future<List<String>> getImages() async {
-    List<String> images = [];
-    String url = "https://pixabay.com/api/?key=$pixabyApiKey&image_type=photo";
-
-    NetworkHelper networkHelper = NetworkHelper(url: url);
-    dynamic data = await networkHelper.getData();
-    List<dynamic> hitsList = data["hits"] as List;
-
-    for (int i = 0; i < hitsList.length; i++) {
-      images.add(hitsList[i]["largeImageURL"]);
-    }
-    return images;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    images = getImages();
-    // getImages().then((value) => print(value));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: images,
-        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              return const Center(child: CircularProgressIndicator());
-            case ConnectionState.done:
-              return SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: GridView.builder(
-                    itemCount: snapshot.data?.length ?? 0,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 6,
-                      mainAxisSpacing: 6,
-                    ),
-                    itemBuilder: (context, index) {
-                      return Image.network(
-                        snapshot.data![index],
-                        fit: BoxFit.cover,
-                      );
-                    },
-                  ),
-                ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: GridView.builder(
+            itemCount: context.watch<GalleryData>().photos.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 6,
+              mainAxisSpacing: 6,
+            ),
+            itemBuilder: (context, index) {
+              return Image.network(
+                context.watch<GalleryData>().photos[index],
+                fit: BoxFit.cover,
               );
-          }
-        },
+            },
+          ),
+        ),
       ),
     );
   }
